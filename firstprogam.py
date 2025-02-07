5da041bca8897b554be6ca8f64de2abf0f31773f
M Bilal <mbilal75362@gmail.com>
2025-02-07 18:24:44 +0500

    python program

---
+++ firstprogam.py
@@ -0,0 +1,87 @@
+# name=input("enter your name:")
+
+# print("length of your name is:",len(name))
+
+
+# marks = int(input("Enter your marks:"))
+
+# if (marks >= 90):
+#     print("Grade A")
+# elif (marks >= 80 and marks < 90):
+#     print("Grade B")
+# elif (marks >= 70 and marks < 80):
+#     print("Grade C")
+# elif (marks >= 60 and marks < 70):
+#     print("Grade D")
+# else:
+#     print("Failed")
+
+# num = int(input("Enter a number:"))
+
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# a= int(input("Enter a number:"))
+# b= int(input("Enter a number:"))
+# c= int(input("Enter a number:"))
+
+# if a>b and a>c:
+#     print("a is greater" ,a)
+# elif b>a and b>c:
+#     print("b is greater", b)
+# else:
+#     print("c is greater", c)
+
+# a= int(input("Enter  number:"))
+
+# if a%7 == 0:
+#     print("divisible by 7")
+# else:
+#     print("not divisible by 7")
+
+# marks = [10, 20, 30, 40, 50]
+# print(marks)
+# marks.append(60)
+# print(marks)
+# marks.insert(2, 25)
+# for i in marks:
+#     print(i)   
+# print(marks[2])
+# marks.remove(30)
+
+
+
+# name = input("Enter moive name:")
+# name2 = input("Enter moive name:")
+# name3 = input("Enter moive name:")
+
+# list = []
+# list.append(name)
+# list.append(name2)
+# list.append(name3)
+# print(list)
+
+
+
+# distionary 
+# dist = {
+#     "name" : "M Bilal",
+#     "age" : 23,
+#     "city" : "Multan",
+#     "country" : "Pakistan",
+#     "marks" : [10, 20, 30, 40, 50],
+#     "movies" : ["movie1", "movie2", "movie3"]
+# }
+
+
+# print(dist)
+
+cout = 0
+
+while cout < 10:
+    print("Hello")
+    cout = cout + 1
+    # print(cout)
+print("End of loop")
